aa4fc3070d85d4123d4625c25fa61b58cb646495
nsikakessien <nsikakessien66@gmail.com>
2022-04-08 04:23:56 -0700

    task 4 c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 
 /*
  * main - Entry point
- *
  * Return: Always 0 {success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }		
